f11587a67a543e7c1955f2cb4d3dc23f883f91f0
cubandle <cubandle@outlook.com>
2023-01-13 17:31:11 -0500

    Backport export

--- app/src/main/java/com/katiearose/sobriety/utils/PreferenceUtils.kt
+++ app/src/main/java/com/katiearose/sobriety/utils/PreferenceUtils.kt
@@ -2,7 +2,10 @@ package com.katiearose.sobriety.utils
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.net.Uri
 import androidx.preference.PreferenceManager
+import com.katiearose.sobriety.activities.Main
+import org.json.JSONArray
 
 fun Context.getSharedPref(): SharedPreferences {
     return PreferenceManager.getDefaultSharedPreferences(this)
@@ -18,4 +21,19 @@ fun SharedPreferences.getSortMilestonesPref(): String {
 
 fun SharedPreferences.getHideCompletedMilestonesPref(): Boolean {
     return getBoolean("hide_completed_milestones", false)
+}
+
+fun Context.exportData(uri: Uri) {
+    // Construct the JSON Object
+    val json: JSONArray = JSONArray();
+    for (i in 0 until Main.addictions.size) {
+        json.put(i, Main.addictions[i].toJSON())
+    }
+
+    // Write to file
+    json.toString(4).byteInputStream().use { input ->
+        contentResolver.openOutputStream(uri)?.use { output ->
+            input.copyTo(output)
+        }
+    }
 }
